d676f9a28a6fe68237b272c2167a05dd1c2e42bf
LMladenovic <lazar.s.mladenovic@hotmail.com>
2020-08-12 11:54:13 +0200

    Add: Multiple file program functionality

--- README.md
+++ README.md
@@ -22,14 +22,9 @@ Pokrenuti komandu
 Pozicionirati se u folderu gde se nalazi glavni fajl <i>koronka.py</i>, kao i pomocni fajlovi u okviru foldera <i>utils</i>.
 
 Alat se pokrece komandom
-<pre> python koronka.py [c file|path to c file] [other arguments] </pre>
+<pre> python koronka.py [files=[list of files]] [c file|path to c file] [other arguments] </pre>
 
 [other arguments] - argumenti koji su potrebni C programu kao argumenti komandne linije ( u trenutnoj verziji projekta)
+[files = [list of files]] - dodatni fajlovi potrebni programu (npr. .h fajlovi)
 
 Alat ce u okviru direktorijuma u kom se nalazi kreirati folder po modelu datumPokretanja-VremePokretanja, u njega kopirati gore navedeni fajl kao argument nad kojim ce vrsiti ispravku, njih menjati, i na kraju generisati ExecutionReport sa izvestajem sta je i na koji nacin promenjeno. Na taj nacin originalni fajl ostaje nepromenjen, a rezultat rada alata i izmenjeni fajl se nalazi u pomenutom folderu. 
-
-### Studenti koji rade na projektu
-
-- [Lazar Mladenovic](https://github.com/LMladenovic)
-- [Tatjana Radovanovic](https://github.com/Tatjana95)
-- [Jovan Maric](https://github.com/maric993)
--- koronka.py
+++ koronka.py
@@ -1,8 +1,9 @@
 from subprocess import call
 from datetime import datetime
-from shutil import copyfile
+from shutil import copyfile, copytree
 import os
 import sys
+import re
 
 # used to avoid writing bytecode, i.e. .pyc files
 sys.dont_write_bytecode = True
@@ -11,6 +12,28 @@ sys.path.append(os.path.abspath(os.getcwd() + "/utils"))
 from parseOutput import *
 from errorHandler import *
 
+def copyNecessaryFiles(directory):
+	# sys.argv[1] possibly contains path to necessary files
+	# sys.argv[2] contain list of necessary files
+	path = ''	
+	if sys.argv[1].rfind('/'):
+		path = sys.argv[1][0:sys.argv[1].rfind('/')+1]
+	files = sys.argv[2][sys.argv[2].find('[')+1::]
+	files = files[0:files.rfind(']')]
+	files = files.split(',')
+	for file in files:
+		if os.path.exists(path + file.strip()):
+			if not os.path.isfile(path + file.strip()):
+				copytree(path + file.strip(), directory + "/" + file.strip())
+				print ("Successfully moved directory " + file.strip())
+			else:		
+				copyfile(path + file.strip(), directory + "/" + file.strip())
+				print ("Successfully moved file " + file.strip())
+		else:
+			print ("The file proceeded as argument [" + path + file.strip() + \
+				"] doesn't exist or is not a file.")
+			return 0
+	return 1					
 
 def compileProgram(filename):
 	# trim .c from filename
@@ -52,6 +75,7 @@ def doJob(filename, clArguments):
 	print("Koronka successfully fixed all that were in her power! :)")
 
 def main():
+	indicator = 1
 	now = datetime.now()
 	current_time = now.strftime("%m%d%Y-%H%M%S")
 
@@ -60,8 +84,14 @@ def main():
 	try:
     		
 		if len(sys.argv)< 2:
-			print ("Usage: python koronka.py PROGRAM [arguments]")
-		elif not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]) :
+			print ("Usage: python koronka.py [files=[list of files]] PROGRAM [arguments]")
+			return
+		if re.search('files=\[.*\]', sys.argv[1]):
+			tmp = sys.argv[1]
+			sys.argv[1]=sys.argv[2]
+			sys.argv[2] = tmp
+			indicator = 0
+		if not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]) :
 			print ("The program proceeded as argument [" + sys.argv[1] + "] doesn't exist or is not a file.")
 			
 		else:
@@ -73,14 +103,18 @@ def main():
 			filename = sys.argv[1].split('/')[-1:][0] 
 
 			copyfile(sys.argv[1], directory + "/" + filename)
-			os.chdir(directory)
 			print ("Successfully created the directory "+ directory +" , and moved file " + filename)
+			if not indicator:
+				indicator = copyNecessaryFiles(directory)
+			os.chdir(directory)
+
 			try:
 				clArguments = ''
-				for i in range(2,len(sys.argv)):
+				for i in range(2+indicator,len(sys.argv)):
 					clArguments += sys.argv[i] + ' '
 				
-				doJob(filename, clArguments) 
+				if indicator:
+					doJob(filename, clArguments) 
 			
 			except OSError:
 				print ("Failed to run koronka :(")
